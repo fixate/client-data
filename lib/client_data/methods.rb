a4c9e5faacc13a05811d303a6b9334ee361bf89c
Stan Bondi <stan@fixate.it>
2014-04-16 22:06:20 +0200

    refactoring, add options hash to client_data

--- lib/client_data/methods.rb
+++ lib/client_data/methods.rb
@@ -4,7 +4,7 @@ module ClientData
 
     included do
       send(:include, ClientData::BeforeRender) unless respond_to?(:before_render)
-      before_render :client_data_filter
+      before_render :client_data_builder_filter
 
       class_eval %(
         class << self; attr_accessor :__cs_builders; end
@@ -12,16 +12,19 @@ module ClientData
     end
 
     module ClassMethods
-      def client_data(*builders)
-        self.__cs_builders ||= []
-        self.__cs_builders.concat(builders).uniq!
+      def client_data(*build_keys)
+        opts = build_keys.extract_options!
+        self.__cs_builders ||= {}
+        build_keys.each do |key|
+          self.__cs_builders[key] = opts
+        end
       end
     end
 
     def builders
       @builders ||= begin
         builders_hash = {}
-        config_keys.each do |key|
+        builder_options_hash.keys.each do |key|
           builders_hash[key] = create_builder(key)
         end
         builders_hash
@@ -30,9 +33,23 @@ module ClientData
 
     protected
 
-    def client_data_filter
-      config_keys.each do |key|
-        provider.set(key, builders[key].build)
+    def client_data_builder_filter
+      builders.each do |key, builder|
+        provider.set(key, builder.build) if should_build_for?(key)
+      end
+    end
+
+    def should_build_for?(key)
+      options = builder_options_hash[key] || {}
+      action = self.action_name.to_sym
+      only = options[:only]
+      if only.nil?
+        true
+      else
+        if only.is_a?(Symbol)
+          only = [only]
+        end
+        only.include?(action)
       end
     end
 
@@ -54,15 +71,15 @@ module ClientData
     end
 
     def provider
-      @provider ||= Adapters.factory(self)
+      @_builder_provider ||= Adapters.factory(self)
     end
 
-    def config_keys
-      @keys ||= begin
-        keys = []
+    def builder_options_hash
+      @_builder_options_hash ||= begin
+        keys = {}
         klass = self.class
         loop do
-          keys += klass.__cs_builders || [] if klass.respond_to?(:__cs_builders)
+          keys.merge!(klass.__cs_builders || {}) if klass.respond_to?(:__cs_builders)
           break keys unless klass.respond_to?(:superclass) && klass = klass.superclass
         end
       end
