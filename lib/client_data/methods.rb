264807a2cacce090b34fdbd0f1d8a6f07d9c0122
Stan Bondi <stan@fixate.it>
2014-04-16 21:24:59 +0200

    Refactored DSL methods

--- lib/client_data/methods.rb
+++ lib/client_data/methods.rb
@@ -1,62 +1,69 @@
 module ClientData
   module Methods
-    def self.included(klass)
-      klass.send(:before_filter, :client_data_filter) if klass.respond_to?(:before_filter)
-      klass.class_eval %(
-          class << self; attr_accessor :__cs_builders; end
+    extend ActiveSupport::Concern
+
+    included do
+      send(:include, ClientData::BeforeRender) unless respond_to?(:before_render)
+      before_render :client_data_filter
+
+      class_eval %(
+        class << self; attr_accessor :__cs_builders; end
       )
-      klass.send(:include, InstanceMethods)
-      klass.send(:extend, ClassMethods)
     end
 
     module ClassMethods
-      def client_data(*configs)
+      def client_data(*builders)
         self.__cs_builders ||= []
-        self.__cs_builders.concat(configs).uniq!
+        self.__cs_builders.concat(builders).uniq!
       end
     end
 
-    module InstanceMethods
-      def client_data_filter
+    def builders
+      @builders ||= begin
+        builders_hash = {}
         config_keys.each do |key|
-          begin
-            klass = "#{builder_namespace}#{key.to_s.capitalize}Builder"
-            klass = klass.constantize
-          rescue ::NameError => e
-            raise ClientData::Error, "Unable to find constant #{klass}, " \
-              "has ClientData.load_resources! been called? " \
-              "Error: #{e.message}"
-          end
-
-          data = build_data(klass)
-          provider.set(key, data)
+          builders_hash[key] = create_builder(key)
         end
+        builders_hash
+      end
+    end
+
+    protected
+
+    def client_data_filter
+      config_keys.each do |key|
+        provider.set(key, builders[key].build)
+      end
+    end
+
+    def create_builder(key)
+      begin
+        klass = "#{builder_namespace}#{key.to_s.capitalize}Builder"
+        klass = klass.constantize
+      rescue ::NameError => e
+        raise ClientData::Error, "Unable to find constant #{klass}, " \
+          "has ClientData.load_resources! been called? " \
+          "Error: #{e.message}"
       end
 
-      protected
+      klass.new.tap { |o| o.controller = self }
+    end
 
-      def build_data(klass)
-        arity = klass.instance_method(:initialize).arity
-        builder = arity == 0 ?  klass.new : klass.new(self)
-        builder.build
-      end
+    def builder_namespace
+      ClientData.configuration.builder_namespace
+    end
 
-      def builder_namespace
-        ClientData.configuration.builder_namespace
-      end
+    def provider
+      @provider ||= Adapters.factory(self)
+    end
 
-      def provider
-        @provider ||= Adapters.factory(self)
-      end
-
-      def config_keys
-        @keys ||= begin
-          keys = []
-          klass = self.class
-          loop do
-            keys += klass.__cs_builders || [] if klass.respond_to?(:__cs_builders)
-            break keys unless klass.respond_to?(:superclass) && klass = klass.superclass
-          end
+    def config_keys
+      @keys ||= begin
+        keys = []
+        klass = self.class
+        loop do
+          keys += klass.__cs_builders || [] if klass.respond_to?(:__cs_builders)
+          break keys unless klass.respond_to?(:superclass) && klass = klass.superclass
         end
       end
     end
